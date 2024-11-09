ba8f1b573719b3abcfd851cf043ff1ad4fab051a
Ashin Habeeb <ashinchulli@gamill.com>
2024-11-09 10:17:39 +0530

    final version of cloud design agency

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        nunito:["Nunito Sans","sans-serif"], 
+        archivo:["Archivo Black","sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
